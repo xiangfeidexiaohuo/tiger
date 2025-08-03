88d4126bbc39b0543e91483f3f16fe1eef1b4a2e
刀刀 <jsjson@163.com>
2025-08-04 01:25:38 +0800

    ok
    Signed-off-by: 刀刀 <jsjson@163.com>

--- diy/lean/lean-a1.sh
+++ diy/lean/lean-a1.sh
@@ -9,6 +9,5 @@ rm -rf ./package/lean/autocore
 sed -i "1isrc-git tiger https://github.com/xiangfeidexiaohuo/tiger\n" feeds.conf.default          
 sed -i "2isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
 
-sed -i 's/#src-git luci/src-git luci/g' feeds.conf.default
-sed -i '/luci.git;openwrt-23.05/d' feeds.conf.default
-sed -i '/luci.git;openwrt-24.10/d' feeds.conf.default
+sed -i 's/luci.git;openwrt-23.05/luci/g' feeds.conf.default
+sed -i 's/luci.git;openwrt-24.10/luci/g' feeds.conf.default
